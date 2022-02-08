995c5acc378becc442460a818269a63d4dd3021b
JoshJDWood <72447249+JoshJDWood@users.noreply.github.com>
2022-02-08 20:49:30 +0000

    first reflection added
    it can now add 1 and only 1 reflection

--- Light in a Box/Assets/Scripts/CastBeam.cs
+++ Light in a Box/Assets/Scripts/CastBeam.cs
@@ -6,27 +6,40 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class CastBeam : MonoBehaviour
 {
     LightBeam2D beam2D;
-    Vector3[] lightPath = { new Vector3(1, 1), new Vector3(1, -1), new Vector3(-1, -1), new Vector3(-1, 1) };
     List<RayData> rayDataSet = new List<RayData>();
     List<Vector2> rayIndices = new List<Vector2>();
     List<Vector3> shapePath = new List<Vector3>();
+
+    LightBeam2D beam2D2;
+    List<Vector3> shapePath2 = new List<Vector3>();
     int bounces = 0;
+
+    Vector3[] lightPath = { new Vector3(1, 1), new Vector3(1, 0), new Vector3(0, 1) };
 
     // Start is called before the first frame update
     void Start()
     {
         Shine(gameObject.transform.position);
-        foreach(RayData idx in rayDataSet)
-        {
-            shapePath.Add(idx.hits[0]);
-        }
+        FindShapePaths();
         beam2D = new LightBeam2D(shapePath.ToArray());
+        beam2D2 = new LightBeam2D(shapePath2.ToArray());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(GameObject.Find("2D Lightbeam"));
+            Destroy(GameObject.Find("2D Lightbeam"));
+            rayDataSet.Clear();
+            shapePath.Clear();
+            shapePath2.Clear();
+            Shine(gameObject.transform.position);
+            FindShapePaths();
+            beam2D = new LightBeam2D(shapePath.ToArray());
+            beam2D2 = new LightBeam2D(shapePath2.ToArray());
+        }
     }
 
     void Shine(Vector2 pos)
@@ -59,6 +72,38 @@ public class CastBeam : MonoBehaviour
             Vector2 nextpos = hit.point + nextdir * 0.0001f;
 
             CastRay(nextpos, nextdir);
+        }
+    }
+
+    void FindShapePaths()
+    {
+        int count = 0;
+        int reflectionS = 0;
+        int reflectionE = 0;
+        bool inreflection = false;
+        foreach (RayData idx in rayDataSet)
+        {
+            if (idx.bounces > 0 && !inreflection)
+            {
+                reflectionS = count;
+                inreflection = true;
+            }
+            else if(idx.bounces == 0 && inreflection)
+            {
+                reflectionE = count;
+                inreflection = false;
+            }
+            shapePath.Add(idx.hits[0]);
+            count++;
+        }
+
+        for (int i = reflectionS; i < reflectionE; i++)
+        {
+            shapePath2.Add(rayDataSet[i].hits[0]);
+        }
+        for (int i = reflectionE - 1; i >= reflectionS; i--)
+        {
+            shapePath2.Add(rayDataSet[i].hits[1]);
         }
     }
 }
